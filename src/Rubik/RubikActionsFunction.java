1c708907dd1226bd534d266d9fb19c105a9981a4
paulamlago <pmunoz06@ucm.es>
2017-11-14 19:49:12 +0100

    Añadido lo del cubo de Rubik

---
+++ src/Rubik/RubikActionsFunction.java
@@ -0,0 +1,17 @@
+package Rubik;
+
+import java.util.Set;
+
+import aima.core.agent.Action;
+
+public class RubikActionsFunction implements aima.core.search.framework.ActionsFunction{
+
+	@Override
+	public Set<Action> actions(Object arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+}
---
+++ src/Rubik/RubikStepCostFunction.java
@@ -0,0 +1,14 @@
+package Rubik;
+
+import aima.core.agent.Action;
+import aima.core.search.framework.StepCostFunction;
+
+public class RubikStepCostFunction implements StepCostFunction{
+
+	@Override
+	public double c(Object arg0, Action arg1, Object arg2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
